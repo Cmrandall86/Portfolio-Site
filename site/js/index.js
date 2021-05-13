3ca814d40a1f1d7e16a49e8fa2d5c5aaf508b076
Chris Randall <cmrandall86@gmail.com>
2021-05-13 15:54:00 -0600

    fixed logo sizing and full screen nav bar on pages

--- site/js/index.js
+++ site/js/index.js
@@ -1,8 +1,10 @@
 let hamburger_icon = document.querySelector('.hamburger');
 let nav_text = document.querySelector('.nav-text');
 let nav_bar = document.querySelector('.nav-bar');
-let closer = document.querySelector('.closer')
-let page_nav_bar = document.querySelector('.page-nav-bar')
+let closer = document.querySelector('.closer');
+// let page_nav_bar = document.querySelector('.page-nav-bar')
+// let page_hamburger = document.querySelector('.page-hamburger');
+// let page_closer = document.querySelector('.page-closer');
 
 hamburger_icon.addEventListener('click', ()=>{
     nav_text.classList.toggle('open')
@@ -10,7 +12,7 @@ hamburger_icon.addEventListener('click', ()=>{
     nav_bar.classList.toggle('open')
     closer.classList.toggle('open')
     nav_bar.classList.toggle('closed')
-
+    // page_nav_bar.classList.toggle('closed')
 })
 
 closer.addEventListener('click' ,()=>{
@@ -19,7 +21,20 @@ closer.addEventListener('click' ,()=>{
     nav_bar.classList.toggle('open')
     nav_text.classList.toggle('open')
     nav_bar.classList.toggle('closed')
-
-
 })
 
+// page_hamburger.addEventListener('click', ()=>{
+//     nav_text.classList.toggle('open')
+//     page_hamburger.classList.toggle('closed')
+//     page_nav_bar.classList.toggle('open')
+//     page-closer.classList.toggle('open')
+//     nav_bar.classList.toggle('closed')
+// })
+
+// page_closer.addEventListener('click' ,()=>{
+//     page_hamburger.classList.toggle('closed')
+//     page-closer.classList.toggle('open')
+//     page_nav_bar.classList.toggle('open')
+//     nav_text.classList.toggle('open')
+//     nav_bar.classList.toggle('closed')
+// })
