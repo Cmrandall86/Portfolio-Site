1570a9a51b8e01582440d241ba82afe13b21a695
Chris Randall <cmrandall86@gmail.com>
2021-05-19 21:53:10 -0600

    trying to fix nav menu

--- site/js/index.js
+++ site/js/index.js
@@ -2,9 +2,8 @@ let hamburger_icon = document.querySelector('.hamburger');
 let nav_text = document.querySelector('.nav-text');
 let nav_bar = document.querySelector('.nav-bar');
 let closer = document.querySelector('.closer');
-// let page_nav_bar = document.querySelector('.page-nav-bar')
-// let page_hamburger = document.querySelector('.page-hamburger');
-// let page_closer = document.querySelector('.page-closer');
+let nav_helper = document.querySelector('.nav_hide')
+
 
 hamburger_icon.addEventListener('click', ()=>{
     nav_text.classList.toggle('open')
@@ -12,7 +11,7 @@ hamburger_icon.addEventListener('click', ()=>{
     nav_bar.classList.toggle('open')
     closer.classList.toggle('open')
     nav_bar.classList.toggle('closed')
-    // page_nav_bar.classList.toggle('closed')
+    nav_helper.classList.toggle('on')
 })
 
 closer.addEventListener('click' ,()=>{
@@ -21,20 +20,7 @@ closer.addEventListener('click' ,()=>{
     nav_bar.classList.toggle('open')
     nav_text.classList.toggle('open')
     nav_bar.classList.toggle('closed')
+    nav_helper.classList.toggle('on')
+
 })
 
-// page_hamburger.addEventListener('click', ()=>{
-//     nav_text.classList.toggle('open')
-//     page_hamburger.classList.toggle('closed')
-//     page_nav_bar.classList.toggle('open')
-//     page-closer.classList.toggle('open')
-//     nav_bar.classList.toggle('closed')
-// })
-
-// page_closer.addEventListener('click' ,()=>{
-//     page_hamburger.classList.toggle('closed')
-//     page-closer.classList.toggle('open')
-//     page_nav_bar.classList.toggle('open')
-//     nav_text.classList.toggle('open')
-//     nav_bar.classList.toggle('closed')
-// })
